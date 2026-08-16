bd52e722df2bfd87e6388aad62a259b9d646e404
Anil Kumar Mahato <anilmahatox13@gmail.com>
2026-08-16 09:30:43 +0530

    fix(models): update User pre-save hook for Mongoose v8 async compatibility

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -47,14 +47,13 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-// Hash password using bcrypt before saving
-userSchema.pre('save', async function (next) {
+// Hash password using bcrypt before saving (Mongoose v8 async hook)
+userSchema.pre('save', async function () {
   if (!this.isModified('password')) {
-    return next();
+    return;
   }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 // Compare password method
